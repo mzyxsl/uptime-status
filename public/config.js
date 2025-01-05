0fd55af2aafc72488afc0ae9b6edb03ec5508b27
名字已消失了 <48387476+mzyxsl@users.noreply.github.com>
2025-01-05 23:30:40 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'mzyxsl Status',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
@@ -20,7 +20,7 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'https://status.mzyxsl.cn/'
     },
     {
       text: 'GitHub',
